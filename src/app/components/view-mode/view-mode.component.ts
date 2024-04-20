b1974ac5e48041d3d1bb3c913a45e174dc681828
amin hasanzadeh <iqson.amin@gmail.com>
2024-04-20 19:32:36 +0300

    get data from http client service

--- src/app/components/view-mode/view-mode.component.ts
+++ src/app/components/view-mode/view-mode.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { HighchartsChartModule } from 'highcharts-angular';
 import Highcharts from 'highcharts/es-modules/masters/highcharts.src';
+import { ChartDataService } from '../../services/chart-data.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-view-mode',
@@ -10,12 +12,49 @@ import Highcharts from 'highcharts/es-modules/masters/highcharts.src';
   styleUrl: './view-mode.component.scss'
 })
 export class ViewModeComponent {
+  dataSubscription!: Subscription;
+  constructor(private chartDataService: ChartDataService) { }
+
+  ngOnInit() {
+    this.getChartData()
+  }
   Highcharts: typeof Highcharts = Highcharts;
+  chartDataDates: string[] = [];
+  chartDataValues: number[] = [];
   chartOptions: Highcharts.Options = {
     series: [{
-      data: [1, 2, 3],
-      type: 'line'
+      data: this.chartDataValues,
+      type: 'spline'
     }]
   };
 
+
+  getChartData(): void {
+    this.dataSubscription = this.chartDataService.getChartData().subscribe({
+      next: (data) => {
+        for (let item of data[1]){
+          this.chartDataDates.push(item.date)
+          this.chartDataValues.push(item.value)
+        }
+      },
+      error: (error) => {
+        console.error(error);
+      },
+      complete: () => {
+        if (this.chartOptions.series && this.chartOptions.series[0]) {
+          this.chartOptions.series = [{
+            data: this.chartDataValues,
+            type: 'spline'
+          }];
+        }
+      }
+    });
+  }
+
+
+  ngOnDestroy(): void {
+    if (this.dataSubscription) {
+      this.dataSubscription.unsubscribe();
+    }
+  }
 }
