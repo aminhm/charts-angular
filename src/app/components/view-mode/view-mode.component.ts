5c652d6eb566de9080fa24b396e66addc011115c
amin hasanzadeh <iqson.amin@gmail.com>
2024-04-20 18:45:12 +0300

    add a simple chart

--- src/app/components/view-mode/view-mode.component.ts
+++ src/app/components/view-mode/view-mode.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { HighchartsChartModule } from 'highcharts-angular';
+import Highcharts from 'highcharts/es-modules/masters/highcharts.src';
 
 @Component({
   selector: 'app-view-mode',
   standalone: true,
-  imports: [],
+  imports: [HighchartsChartModule],
   templateUrl: './view-mode.component.html',
   styleUrl: './view-mode.component.scss'
 })
 export class ViewModeComponent {
+  Highcharts: typeof Highcharts = Highcharts;
+  chartOptions: Highcharts.Options = {
+    series: [{
+      data: [1, 2, 3],
+      type: 'line'
+    }]
+  };
 
 }
