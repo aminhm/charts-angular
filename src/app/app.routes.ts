f04b4be5f30362ee9e2cb5b021b1a1789f5c4627
amin hasanzadeh <iqson.amin@gmail.com>
2024-04-22 15:10:03 +0300

    settings component and routes added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { ViewModeComponent } from './components/view-mode/view-mode.component';
+import { SettingsComponent } from './components/settings/settings.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component: ViewModeComponent  
+    },
+    {
+        path: 'settings', component: SettingsComponent  
+    }
+];
