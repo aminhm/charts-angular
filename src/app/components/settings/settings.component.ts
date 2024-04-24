096ab5a1a3fe087359fe1ebc8d90a709a024c2be
amin hasanzadeh <iqson.amin@gmail.com>
2024-04-24 14:59:22 +0300

    setting with list of charts added

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
+import { UtilsService } from '../../services/utils.service';
+import { Subscription } from 'rxjs';
+import { AddChartComponent } from '../add-chart/add-chart.component';
+import { ColorPickerComponent } from '../color-picker/color-picker.component';
+import { ViewModeComponent } from '../view-mode/view-mode.component';
+import { ChartDataModel } from '../../models/chart-data.model';
+import { CommonModule } from '@angular/common';
+import { ChartComponent } from '../chart/chart.component';
+import { TuiButtonModule } from '@taiga-ui/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule,
+    AddChartComponent,
+    CommonModule,
+    ChartComponent,
+    TuiButtonModule,
+  ],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss'
 })
 export class SettingsComponent {
+  subscription: Subscription;
+  chartDataModels: ChartDataModel[] = [];
 
+  constructor(public utilsService: UtilsService) {
+    this.subscription = this.utilsService.chartDataModelsItems$.subscribe(data => {
+      this.chartDataModels = data
+    });
+  }
+
+  removeChart(index:number){
+    this.chartDataModels.splice(index,1)
+    this.utilsService.setChartDataModels(this.chartDataModels)
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
