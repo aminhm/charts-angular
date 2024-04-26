d8aea8c7103017a2afbb0f1dec9e0c1ab5844412
amin hasanzadeh <iqson.amin@gmail.com>
2024-04-26 22:20:12 +0300

    finalize view mode

---
+++ src/app/components/pages/view-mode/view-mode.component.ts
@@ -0,0 +1,30 @@
+import { Component} from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { ChartSelectors } from '../../../ngxs/chart/chart.selector';
+import { Select } from '@ngxs/store';
+import { ChartModel } from '../../../ngxs/chart/chart.model';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { TuiInputModule, TuiInputRangeModule } from '@taiga-ui/kit';
+import { ChartComponent } from '../../shared/chart/chart.component';
+import { DateFilterComponent } from '../../utils/date-filter/date-filter.component';
+
+@Component({
+  selector: 'app-view-mode',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    TuiInputModule,
+    RouterModule,
+  ChartComponent,
+  CommonModule,
+  TuiInputRangeModule,
+  DateFilterComponent
+],
+  templateUrl: './view-mode.component.html',
+  styleUrl: './view-mode.component.scss',
+})
+export class ViewModeComponent {
+  @Select(ChartSelectors.chartItems) chartItems$!: Observable<ChartModel[]>;
+}
